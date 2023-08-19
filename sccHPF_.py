fdc944dfb43732be6ef1407edba30de22a5a34a9
nicholashou27 <32993983+nicholashou27@users.noreply.github.com>
2023-08-19 14:15:21 -0400

    Update sccHPF_.py

--- sccHPF_.py
+++ sccHPF_.py
@@ -289,7 +289,7 @@ class cNMF():
 
         adata = sc.AnnData(X)
         
-        hpf_model = schpf.run_trials(sp.coo_matrix(adata.X),nmf_kwargs['n_components'],ntrials=1,epsilon=0.1)
+        hpf_model = schpf.run_trials(sp.coo_matrix(adata.X),nmf_kwargs['n_components'],ntrials=1,epsilon=0.01)
         (W, H) = hpf_model.cell_score(), hpf_model.gene_score()
 
         usages = pd.DataFrame(W, index=X.index, columns=topic_labels)
