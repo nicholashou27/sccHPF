9f076c2a414859413299a2c9b8a9d58e32be4972
nicholashou27 <32993983+nicholashou27@users.noreply.github.com>
2023-09-08 14:00:56 -0400

    Update sccHPF_.py

--- sccHPF_.py
+++ sccHPF_.py
@@ -12,12 +12,13 @@ import scanpy as sc
 from scipy.spatial.distance import squareform
 from sklearn.decomposition import non_negative_factorization
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score, silhouette_samples
 
 from fastcluster import linkage
 from scipy.cluster.hierarchy import leaves_list
 
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 def save_df_to_npz(obj, filename):
     np.savez_compressed(filename, data=obj.values, index=obj.index.values, columns=obj.columns.values)
@@ -451,6 +452,51 @@ class cNMF():
         # Compute the silhouette score
         stability = silhouette_score(l2_spectra.values, kmeans_cluster_labels, metric='euclidean')
 
+        # Silhouette plot
+        
+        fig, ax1 = plt.subplots(1, 1)
+        fig.set_size_inches(18, 7)
+        ax1.set_xlim([-1, 1])
+        ax1.set_ylim([0, len(l2_spectra.values) + (k + 1) * 10])
+        
+        sample_silhouette_values = silhouette_samples(l2_spectra.values, kmeans_cluster_labels)
+        y_lower = 10
+        for i in range(k):
+            # Aggregate the silhouette scores for samples belonging to
+            # cluster i, and sort them
+            ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+    
+            ith_cluster_silhouette_values.sort()
+    
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+    
+            color = cm.nipy_spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(
+                np.arange(y_lower, y_upper),
+                0,
+                ith_cluster_silhouette_values,
+                facecolor=color,
+                edgecolor=color,
+                alpha=0.7,
+            )
+    
+            # Label the silhouette plots with their cluster numbers at the middle
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+    
+            # Compute the new y_lower for next plot
+            y_lower = y_upper + 10  # 10 for the 0 samples
+    
+        ax1.set_title("The silhouette plot for K factors.")
+        ax1.set_xlabel("The silhouette coefficient values")
+        ax1.set_ylabel("Factor")
+    
+        # The vertical line for average silhouette score of all the values
+        ax1.axvline(x=stability, color="red", linestyle="--")
+    
+        ax1.set_yticks([])  # Clear the yaxis labels / ticks
+        ax1.set_xticks([-1,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.4,0.6,0.8,1])
+
         # Obtain the reconstructed count matrix by re-fitting the usage matrix and computing the dot product: usage.dot(spectra)
         refit_nmf_kwargs = dict(
             n_components = k,
@@ -624,13 +670,13 @@ class cNMF():
 
 
         ax1.plot(stats.k, stats.stability, 'o-', color='b')
-        ax1.set_ylabel('Stability', color='b', fontsize=15)
+        ax1.set_ylabel('Silhouette Score', color='b', fontsize=15)
         for tl in ax1.get_yticklabels():
             tl.set_color('b')
         #ax1.set_xlabel('K', fontsize=15)
 
         ax2.plot(stats.k, stats.prediction_error, 'o-', color='r')
-        ax2.set_ylabel('Error', color='r', fontsize=15)
+        ax2.set_ylabel('Frobenius Error', color='r', fontsize=15)
         for tl in ax2.get_yticklabels():
             tl.set_color('r')
 
