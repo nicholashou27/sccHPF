6685f51da6ea4e6919a22804e92043a4c27bb1b1
nicholashou27 <32993983+nicholashou27@users.noreply.github.com>
2023-10-12 21:02:25 -0400

    Update sccHPF_.py

--- sccHPF_.py
+++ sccHPF_.py
@@ -415,7 +415,7 @@ class cNMF():
 
     def consensus(self, k, density_threshold_str='0.5', local_neighborhood_size = 0.30,show_clustering = False, skip_density_and_return_after_stats = False, close_clustergram_fig=True,
                 train_set=False, # EDIT 10/12/23
-                train_X=None # EDIT 10/12/23):
+                train_X=None): # EDIT 10/12/23
         merged_spectra = load_df_from_npz(self.paths['merged_spectra']%k)
         norm_counts = load_df_from_npz(self.paths['normalized_counts'])
 
